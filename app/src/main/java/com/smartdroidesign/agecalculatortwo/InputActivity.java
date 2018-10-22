0f0f10bebadfcf83297024c0257fbb789addec01
Memnoc <smartdroidesign@gmail.com>
2018-10-22 14:03:05 +0100

    Initial commit

---
+++ app/src/main/java/com/smartdroidesign/agecalculatortwo/InputActivity.java
@@ -0,0 +1,49 @@
+package com.smartdroidesign.agecalculatortwo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class InputActivity extends AppCompatActivity {
+
+    private TextView birthdayHeaderInput;
+    private TextView currentFooterInput;
+
+    private EditText birthdayFieldInput;
+    private EditText currentFieldInput;
+
+    private String birthdayDate;
+    private String currentDate;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+        birthdayHeaderInput = findViewById(R.id.header_txt_input);
+        currentFooterInput = findViewById(R.id.header_two_txt_input);
+
+        birthdayFieldInput = findViewById(R.id.birthday_txt_input);
+        currentFieldInput = findViewById(R.id.current_txt_input);
+
+        Button calculateButtonInput = findViewById(R.id.calculateBtnInput);
+
+        calculateButtonInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InputActivity.this, CalculateActivity.class);
+                birthdayDate = birthdayFieldInput.getText().toString().trim();
+                currentDate = currentFieldInput.getText().toString().trim();
+                User user = new User(birthdayDate, currentDate);
+                intent.putExtra("userData", user);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/smartdroidesign/agecalculatortwo/User.java
@@ -0,0 +1,59 @@
+package com.smartdroidesign.agecalculatortwo;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
+
+    private String userBirthday;
+    private String currentDate;
+
+    public User(String userBirthday, String currentDate) {
+        this.userBirthday = userBirthday;
+        this.currentDate = currentDate;
+    }
+
+    protected User(Parcel in) {
+        userBirthday = in.readString();
+        currentDate = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public String getUserBirthday() {
+        return userBirthday;
+    }
+
+    public void setUserBirthday(String userBirthday) {
+        this.userBirthday = userBirthday;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(userBirthday);
+        dest.writeString(currentDate);
+    }
+}
